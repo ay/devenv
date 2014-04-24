2597eae8d88b2f8a724d0a8652f0243ea97c3493
Alex Yakoubian <alex@alexyakoubian.com>
2014-04-24 00:13:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# devenv
+
+devenv is a tiny bash script that lets you quickly bootstrap a local development environment with the latest stable versions of Ruby with [rbenv](https://github.com/sstephenson/rbenv), Node with [nvm](https://github.com/creationix/nvm), Python with [pyenv](https://github.com/yyuu/pyenv), and Go. Everything is installed **locally** in your home directory.
+
+## Requirements
+
+I've only tested this on recent versions of Ubuntu and OS X Mavericks. You need:
+
+  * Git
+  * curl
+  * **Ubuntu**:
+    * `build-essential` package
+    * `libssl-dev` package
+  * **OS X**: Command Line Tools (`xcode-select --install`)
+
+## Installation
+
+Before installing, you should read through [install.sh](install.sh). You could either clone this repo and run install.sh from there or just:
+
+```
+$ curl https://raw.github.com/alexyakoubian/devenv/install.sh | sh
+```
+
+### Ruby / rbenv
+
+Ruby is installed with rbenv and [ruby-build](https://github.com/sstephenson/ruby-build). rbenv is installed in `~/.rbenv` and the latest stable Ruby under its version directory in `~/.rbenv/versions`. This Ruby install is also set as rbenv's global Ruby version with `rbenv global`. For rbenv to work properly, you need to add these to your shell environment:
+
+```sh
+export PATH="$HOME/.rbenv/bin:PATH"
+eval "$(rbenv init -)"
+```
+
+### Node / nvm
+
+Node is installed with nvm, which is installed in `~/.nvm`. The latest stable Node is installed under its version directory in `~/.nvm` and made the default Node version with `nvm alias default`. For nvm to work properly you need to load `~/.nvm/nvm.sh` into your shell environment:
+
+```sh
+source ~/.nvm/nvm.sh
+```
+
+### Python / pyenv
+
+Python is installed with pyenv, which is installed in `~/.pyenv`. The latest stable Python is installed under its version directory in `~/.pyenv/versions`. This Python install is also set as pyenv's global Python version with `pyenv global`. For pyenv to work properly, you need to add these to your shell environment:
+
+```sh
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+```
+
+### Go
+
+Go is installed from an official [binary tarball](https://code.google.com/p/go/downloads/list?q=OpSys-FreeBSD+OR+OpSys-Linux+OR+OpSys-OSX+Type-Archive) for your system into `~/.local/go`. `~/.go` is also created to use as your GOPATH. For Go to work properly, you need to add these to your shell environment:
+
+```sh
+export GOROOT="$HOME/.local/go"
+export PATH="$PATH:$GOROOT/bin"
+export GOPATH="$HOME/.go"
+export PATH="$PATH:$GOPATH/bin"
+```
---
+++ install.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+
+ruby_version="2.1.1"
+node_version="0.10.26"
+python_version="2.7.6"
+go_version="1.2.1"
+
+green () { printf "\033[32m$1\033[0m\n"; }
+yellow () { printf "\033[33m$1\033[0m\n"; }
+red () { printf "\033[31m$1\033[0m\n"; }
+
+ask () {
+    local question="$1" default_y="$2" yn
+    if [ -z "$default_y" ]; then
+        read -p "$question [y/N]? "
+    else
+        read -p "$question [Y/n]? "
+    fi
+    yn=$(echo "$REPLY" | tr "A-Z" "a-z")
+    if [ -z "$default_y" ]; then
+        test "$yn" == "y" -o "$yn" == 'yes'
+    else
+        test "$yn" != 'n' -a "$yn" != 'no'
+    fi
+}
+
+# Install rbenv and Ruby
+if [ ! -e "$HOME/.rbenv" ]; then
+    if ask "Install rbenv and Ruby" "Y"; then
+        yellow "==> Installing rbenv into ~/.rbenv"
+        git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+        git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+        export PATH="$HOME/.rbenv/bin:$PATH"
+        eval "$(rbenv init -)"
+        yellow "==> Compiling and installing Ruby v$ruby_version"
+        rbenv install $ruby_version
+        rbenv global $ruby_version
+        rbenv_installed=true
+    fi
+else
+    red "==> ~/.rbenv already exists"
+fi
+
+# Install nvm and Node
+if [ ! -e "$HOME/.nvm" ]; then
+    if ask "Install nvm and Node" "Y"; then
+        yellow "==> Installing nvm into ~/.nvm"
+        git clone https://github.com/creationix/nvm.git ~/.nvm
+        source ~/.nvm/nvm.sh
+        yellow "==> Installing Node v$node_version"
+        nvm install $node_version
+        nvm alias default $node_version
+        nvm_installed=true
+    fi
+else
+    red "==> ~/.nvm already exists"
+fi
+
+# Install pyenv and Python
+if [ ! -e "$HOME/.pyenv" ]; then
+    if ask "Install pyenv and Python" "Y"; then
+        yellow "==> Installing pyenv into ~/.pyenv"
+        git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+        export PYENV_ROOT="$HOME/.pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        yellow "==> Installing Python v$python_version"
+        pyenv install $python_version
+        pyenv global $python_version
+        pyenv rehash
+        yellow "==> Installing virtualenv"
+        $HOME/.pyenv/shims/pip install virtualenv
+        yellow "==> Installing virtualenvwrapper"
+        $HOME/.pyenv/shims/pip install virtualenvwrapper
+        pyenv_installed=true
+    fi
+else
+    red "==> ~/.pyenv already exists"
+fi
+
+# Install Go
+if [ ! -e "$HOME/.local/go" ]; then
+    if ask "Install Go to ~/.local/go" "Y"; then
+        yellow "==> Installing Go v$go_version to ~/.local/go"
+
+        # Determine binary tarball filename
+        platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+        if [ "$(uname -m)" = "x86_64" ]; then
+            arch="amd64"
+        else
+            arch="386"
+        fi
+        if [ "$platform" = "darwin" ]; then
+            if [ "$(uname -r)" \> "12" ]; then
+                extra="-osx10.8"
+            else
+                extra="-osx10.6"
+            fi
+        fi
+        tarball="go${go_version}.${platform}-${arch}${extra}.tar.gz"
+
+        # Download URL
+        download="https://go.googlecode.com/files/$tarball"
+
+        # Attempt to download and untar
+        yellow "==> Downloading Go binary tarball from $download"
+        curl -L -f -C - --progress-bar "$download" -o "/tmp/$tarball"
+        if [ $? -eq 0 ]; then
+            yellow "==> Untarring $tarball to ~/.local/go"
+            mkdir -p "$HOME/.local/go"
+            tar zxf "/tmp/$tarball" --strip-components 1 -C "$HOME/.local/go"
+            rm "/tmp/$tarball"
+            yellow "==> Creating ~/.go to use as GOPATH"
+            mkdir -p $HOME/.go
+            go_installed=true
+        else
+            red "==> Go binary tarball download failed"
+        fi
+    fi
+else
+    red "==> ~/.local/go already exists"
+fi
+
+if [ "$rbenv_installed" = true ]; then
+    green "
+Ruby v$ruby_version is now installed with rbenv in ~/.rbenv. You should add
+these to your shell environment:
+
+    export PATH=\"\$HOME/.rbenv/bin:\$PATH\"
+    eval \"\$(rbenv init -)\"
+    "
+fi
+
+if [ "$nvm_installed" = true ]; then
+    green "
+Node v$node_version is now installed with nvm in ~/.nvm. You should add these
+to your shell environment:
+
+    source ~/.nvm/nvm.sh
+    "
+fi
+
+if [ "$pyenv_installed" = true ]; then
+    green "
+Python v$python_version is now installed with pyenv in ~/.pyenv. You should add
+these to your shell environment:
+
+    export PYENV_ROOT=\"\$HOME/.pyenv\"
+    export PATH=\"\$PYENV_ROOT/bin:\$PATH\"
+    eval \"\$(pyenv init -)\"
+    "
+fi
+
+if [ "$go_installed" = true ]; then
+    green "
+Go v$go_version is now installed in ~/.local/go. ~/.go was also created to use
+as your GOPATH. You should add these to your shell environment:
+
+    export GOROOT=\"\$HOME/.local/go\"
+    export PATH=\"\$PATH:\$GOROOT/bin\"
+    export GOPATH=\"\$HOME/.go\"
+    export PATH=\"\$PATH:\$GOPATH/bin\"
+    "
+fi
