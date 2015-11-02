4304245f8e676d3808857b7cba42cf6df0d9e655
Alex Yakoubian <alex@alexyakoubian.com>
2015-11-01 22:53:00 -0800

    Securely download pyenv

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ fi
 if [ ! -e "${HOME}/.pyenv" ]; then
     if ask "Install pyenv and Python" "Y"; then
         yellow "==> Installing pyenv into ~/.pyenv"
-        git clone git://github.com/yyuu/pyenv.git "${HOME}/.pyenv"
+        git clone https://github.com/yyuu/pyenv.git "${HOME}/.pyenv"
         export PYENV_ROOT="${HOME}/.pyenv"
         export PATH="${PYENV_ROOT}/bin:${PATH}"
         eval "$(pyenv init -)"
